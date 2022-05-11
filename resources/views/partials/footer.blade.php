0a2badfc2096e08f0c6249b2b8e1ce2a5e02473c
LaSe28 <mattia.las28@gmail.com>
2022-05-11 17:22:20 +0200

    end

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -4,6 +4,7 @@
     <div class="footer-container">
         <div class="list">
             <ul>
+                <h3>menu 1</h3>
                 <li>contatti</li>
                 <li>lavora con noi</li>
                 <li>messaggi</li>
@@ -11,6 +12,7 @@
                 <li>social</li>
             </ul>
             <ul>
+                <h3>menu 2</h3>
                 <li>contatti</li>
                 <li>lavora con noi</li>
                 <li>messaggi</li>
@@ -18,12 +20,17 @@
                 <li>social</li>
             </ul>
             <ul>
+                <h3>menu 3</h3>
                 <li>contatti</li>
                 <li>lavora con noi</li>
                 <li>messaggi</li>
                 <li>il nostro shop</li>
                 <li>social</li>
             </ul>
+        </div>
+        <div class="images">
+            <img src="{{asset('images/dc.jpeg')}}" alt="">
+            <img src="{{asset('images/marvel.png')}}" alt="">
         </div>
     </div>
 </footer>
